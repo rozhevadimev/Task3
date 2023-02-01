ef3c02f36884b28e374a76b7e53a9419954350c2
vinnikov <vinnikov@gim16.local>
2023-02-01 10:14:14 +0700

    Init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//Задание 6.
+
+Console.WriteLine("Please, Enter number");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 !=0)
+        
+Console.WriteLine($"The number is odd");
+
+else
+        
+Console.WriteLine($"The number is even");
+  
